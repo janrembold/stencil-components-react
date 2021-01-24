42ef73a06de057674b7b5e5919999bdb7a854566
janrembold <janrembold@gmx.net>
2021-01-24 21:34:27 +0100

    initial draft commit

---
+++ dist/components.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+export const MyComponent = /*@__PURE__*/ createReactComponent('my-component');
+//# sourceMappingURL=components.js.map

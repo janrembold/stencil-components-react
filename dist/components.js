20144fd49d6791315748d9dde65e4e86c5908460
janrembold <janrembold@gmx.net>
2021-01-25 17:32:03 +0100

    update to 0.0.6

--- dist/components.js
+++ dist/components.js
@@ -2,5 +2,6 @@
 /* tslint:disable */
 /* auto-generated react proxies */
 import { createReactComponent } from './react-component-lib';
+export const HmgContainer = /*@__PURE__*/ createReactComponent('hmg-container');
 export const MyComponent = /*@__PURE__*/ createReactComponent('my-component');
 //# sourceMappingURL=components.js.map
--- src/components.ts
+++ src/components.ts
@@ -7,4 +7,5 @@ import type { JSX } from 'stencil-components';
 
 
 
+export const HmgContainer = /*@__PURE__*/createReactComponent<JSX.HmgContainer, HTMLHmgContainerElement>('hmg-container');
 export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
